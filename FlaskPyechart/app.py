f0ea7688ade46fe7ff67e4fb7f660de78e1496f4
KingsleyDeng <460556933@qq.com>
2020-03-18 20:16:50 +0800

    Flask-Pyecharts 增量更新图表

--- FlaskPyechart/app.py
+++ FlaskPyechart/app.py
@@ -1,10 +1,10 @@
 from random import randrange
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 from pyecharts import options as opts
 from pyecharts.charts import Bar
-
+from pyecharts.charts import Line
 
 app = Flask(__name__, static_folder="templates")
 
@@ -12,10 +12,10 @@ app = Flask(__name__, static_folder="templates")
 def bar_base() -> Bar:
     c = (
         Bar()
-        .add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
-        .add_yaxis("商家A", [randrange(0, 100) for _ in range(6)])
-        .add_yaxis("商家B", [randrange(0, 100) for _ in range(6)])
-        .set_global_opts(title_opts=opts.TitleOpts(title="Bar-基本示例", subtitle="我是副标题"))
+            .add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
+            .add_yaxis("商家A", [randrange(0, 100) for _ in range(6)])
+            .add_yaxis("商家B", [randrange(0, 100) for _ in range(6)])
+            .set_global_opts(title_opts=opts.TitleOpts(title="Bar-基本示例", subtitle="我是副标题"))
     )
     return c
 
@@ -31,5 +31,36 @@ def get_bar_chart():
     return c.dump_options_with_quotes()
 
 
+def line_base() -> Line:
+    line = (
+        Line()
+            .add_xaxis(["{}".format(i) for i in range(10)])
+            .add_yaxis(
+            series_name="",
+            y_axis=[randrange(50, 80) for _ in range(10)],
+            is_smooth=True,
+            label_opts=opts.LabelOpts(is_show=False),
+        )
+            .set_global_opts(
+            title_opts=opts.TitleOpts(title="动态数据"),
+            xaxis_opts=opts.AxisOpts(type_="value"),
+            yaxis_opts=opts.AxisOpts(type_="value"),
+        )
+    )
+    return line
+
+@app.route("/lineChart")
+def get_line_chart():
+    c = line_base()
+    return c.dump_options_with_quotes()
+
+idx = 9
+
+@app.route("/lineDynamicData")
+def update_line_data():
+    global idx
+    idx = idx + 1
+    return jsonify({"name": idx, "value": randrange(50, 80)})
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
